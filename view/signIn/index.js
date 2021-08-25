074b0b403e42b53df1ceee0c9721cef68243704f
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2021-08-24 22:58:54 -0300

    24.08.21 - Messages of Error

--- view/signIn/index.js
+++ view/signIn/index.js
@@ -11,22 +11,29 @@ const SignIn = async (SU_NICKNAME, SU_PASSWORD) => {
                 },
                 body: JSON.stringify({SU_NICKNAME, SU_PASSWORD})
             });
-
             const json = await req.json();
 
             if(json.token) {
-                window.location.replace('../home/index.html');
+                window.location.replace('../home/index.html', { json });
             } else {
-                alert('Seu nickname/senha estão incorretos. Por favor, tente novamente!');
+                const setMessage = document.getElementById('alerta');
+                setMessage.style.display = 'flex';
+                setMessage.innerHTML = `<input type="text" value="${json.mensagem}" style="background: #ed5565; color: white; font-size: 1rem;" >`;
+                setTimeout(function() {
+                    setMessage.style.display = 'none';
+                }, 3000);
             }
         } else {
-            alert('Preencha os campos!');
+            const setMessage = document.getElementById('alerta');
+            setMessage.style.display = 'flex';
+            setMessage.innerHTML = `<input type="text" value="Preencha os campos!" style="background: #ed5565; color: white; font-size: 1rem;" >`;
+            setTimeout(function() {
+                setMessage.style.display = 'none';
+            }, 3000);
         }
     } catch (err) {
         alert(err);
     }
 }
-
-
 
 
