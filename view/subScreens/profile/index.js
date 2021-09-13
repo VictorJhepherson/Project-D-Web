3b917f41473b536f5481ffbc880707fb2b5345c1
Lucas Simões <ssl.lucas.simoes@gmail.com>
2021-09-12 21:12:33 -0300

    Added 'add user' option

---
+++ view/subScreens/profile/index.js
@@ -0,0 +1,45 @@
+const BASE_API = 'https://project-d-api.herokuapp.com';
+
+const Register = async (SU_NICKNAME, SU_LOGINNAME, SU_PASSWORD, SU_PHONENUMBER, SU_DATEBIRTHDAY, SU_PHOTO) => {
+    try {
+        if(SU_NICKNAME != '' && SU_LOGINNAME != '' && SU_PASSWORD != '' && SU_PHONENUMBER != '' && SU_DATEBIRTHDAY != '') {
+            const req = await fetch(`${BASE_API}/auth/register`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({SU_NICKNAME, SU_LOGINNAME, SU_PASSWORD, SU_PHONENUMBER, SU_DATEBIRTHDAY, SU_PHOTO})
+            });
+            const json = await req.json();
+
+            if(json.token) {
+                alert("Usuário cadastrado.");
+
+                console.log(json.data.SU_ID + "" + json.data.SU_NICKNAME);
+
+                window.localStorage.setItem('user', json.data.SU_ID.toString());
+                window.localStorage.setItem('token', json.token);
+                window.location.replace('../home/index.html', { json });
+            } else {
+                const setMessage = document.getElementById('alerta');
+                setMessage.style.display = 'flex';
+                setMessage.innerHTML = `<input type="text" value="${json.mensagem}" style="background: #ed5565; color: white; font-size: 1rem;" >`;
+                setTimeout(function() {
+                    setMessage.style.display = 'none';
+                }, 3000);
+            }
+        } else {
+            const setMessage = document.getElementById('alerta');
+            setMessage.style.display = 'flex';
+            setMessage.innerHTML = `<p style="background: #ed5565; color: white; font-size: 1rem;" > Reveja os campos. </p>`;
+            setTimeout(function() {
+                setMessage.style.display = 'none';
+            }, 3000);
+        }
+    } catch (err) {
+        alert(err);
+    }
+}
+
+
